1a407d8d9e6e2be62723395c34aec15472143cea
Lauren P Marazzi <laurenmarazzi@gmail.com>
2019-08-21 15:29:26 -0400

    Add files via upload

--- RNA-SeqGOAnalysis.R
+++ RNA-SeqGOAnalysis.R
@@ -4,9 +4,8 @@
 #---------------------------------------------------------------------------
 # this code shows complete analysis used in the paper in following 6 step:
 #---------------------------------------------------------------------------
-#step1: reading raw counts data and sample information
 #step2: pre-processing data
-#step3: quality control: MDS plot, PCA plot and dendogram
+#step3: quality control: MDS plot
 #step4: correlation analysis
 #step5: DEG analysis using Limma
 #step6: Go terms for DEGs
@@ -15,7 +14,7 @@
 ##Dept. of Mathematics and Statistics
 ##Connecticut College
 ##New London, CT, 06320
-##Last Updated on: 7/19/2019
+##Last Updated on: 8/13/2019
 ############################################################################
 
 ##Load required R packages
@@ -31,7 +30,7 @@ data <- read.table("Second2017_counts.counts.matrix", header=T, row.names=1, com
 sampleinfo <- read.table("sampleinfo.txt",header = TRUE)
 names(data)
 ##naming stages and replicates
-names(data) <- c("37K","37L","37M","38A","38B","38C","39D","39E","39F","39G","39N","39O","41H","41J","41P","41Q","41R")
+names(data) <- c("37/38K","37/38L","37/38M","37/38A","37/38B","37/38C","39D","39E","39F","39G","39N","39O","41H","41J","41P","41Q","41R")
 ##removing 43S and T for the quality control analysis
 data <- data[,-c(18,19)]
 #remove corresponding stage from sampleinfo
@@ -59,9 +58,18 @@ title("Barplot of library sizes")
 y <- calcNormFactors(y)
 #get log2 counts per million
 logcounts <- cpm(y,log=TRUE)
-barplot(logcounts,names=colnames(y),las=2,xlab="Sample",ylab="Library Size",cex.axis = 0.7,cex.names = 0.7)
+##the logged version of barplot takes a while
+#barplot(logcounts,names=colnames(y),las=2,xlab="Sample",ylab="Library Size",cex.axis = 0.7,cex.names = 0.7)
 normalized.counts <- cpm(y)
 transposed <- t(logcounts)
+#par(mfrow=c(2,1))
+#barplot(y$samples$lib.size,names=colnames(y),las=2,xlab="Sample",ylab="Library Size",cex.axis = 0.7,cex.names = 0.7,yaxt="n")
+#axis(2,at=c(0,100000,200000,300000,400000,500000,600000,700000),labels=c("0","100,000","200,000","300,000","400,000","500,000","600,000","700,000"),cex.axis=0.7)
+#title("Barplot of raw library sizes")
+#barplot(logcounts,names=colnames(y),las=2,xlab="Sample",ylab="Library Size",cex.axis = 0.7,cex.names = 0.7)
+#axis(2,at=c(0,10000,20000,30000,40000,50000,60000),labels=c("0","10,000","20,000","30,000","40,000","50,000","60,000"),cex.axis=0.7,yaxt="n")
+#title("Barplot of log2CPM library sizes")
+
 
 #######################################################################
 ## Step3: Quality Control ##
@@ -72,64 +80,11 @@ levels(as.factor(samplR))
 
 col.cell<-c(1,2,3)[as.factor(samplR)]
 data.frame(as.factor(samplR),col.cell)
-plotMDS(y,col=col.cell,gene.selection = "pairwise",cex=1)
-legend("top",horiz=T,legend=levels(as.factor(samplR)),fill=c(1,2,3,4,5),cex=1)
-title("(a)")
+##MDS plot of logcounts
+plotMDS(logcounts,col=col.cell,gene.selection = "pairwise",cex=0.7)
+legend("topleft",horiz=T,legend=levels(as.factor(samplR)),fill=c(1,2,3,4,5),cex=0.7)
+title("MDS Plot")
 
-##2. PCA Plot
-pca <- prcomp(t(logcounts), scale = T)
-plot(pca)
-summary(pca)
-loadings <- pca$rotation
-scores <- pca$x
-
-plot(scores[,1], scores[,2], main="(b)",
-     xlab=paste0("PC1: ", round(summary(pca)$importance[2,1],3)*100, "% variance explained"), 
-     ## indicate the % variance explained by PC1
-     ylab=paste0("PC2: ", round(summary(pca)$importance[2,2],3)*100, "% variance explained"), 
-     ## indicate the % variance explained by PC2
-     pch=16,col=col.cell,cex=1.5,
-     ## points shape according to treatment
-     xlim=c(min(scores[,1]), max(scores[,1])) ,
-     ylim=c(min(scores[,2]), max(scores[,2])+(max(scores[,2])-min(scores[,2]))/4) 
-     ## Let a bit of room on top of the plot for legend
-)
-data2 <- data
-names(data2)[1:6]<-c("37K", "37L","37M","38A","38B","38C")
-text(scores[,1], scores[,2] + 5, names(data2),cex=0.8)
-legend("top", horiz=T, legend=levels(as.factor(samplR)),fill=c(1,2,3,4,5),cex=0.8)
-
-##3. Dendogram
-distance <- dist(transposed)
-clusters <- hclust(distance)
-plot(clusters,main="(c)",ylab="Euclidean Distance",xlab=NA,sub=NA)
-
-##All quality control plots together
-par(mfrow=c(1,3))
-##MDS
-samplR<-c(rep("37/38",6),rep(39,6),rep(41,5))
-levels(as.factor(samplR))
-col.cell<-c(1,2,3)[as.factor(samplR)]
-data.frame(as.factor(samplR),col.cell)
-plotMDS(y,col=col.cell,gene.selection = "pairwise",cex=1.2)
-legend("top",horiz=T,legend=levels(as.factor(samplR)),fill=c(1,2,3,4,5),cex=1)
-title("(a)")
-##PCA
-plot(scores[,1], scores[,2], main="(b)",
-     xlab=paste0("PC1: ", round(summary(pca)$importance[2,1],3)*100, "% variance explained"), 
-     ## indicate the % variance explained by PC1
-     ylab=paste0("PC2: ", round(summary(pca)$importance[2,2],3)*100, "% variance explained"), 
-     ## indicate the % variance explained by PC2
-     pch=16,col=col.cell,cex=1.5,
-     ## points shape according to treatment
-     xlim=c(min(scores[,1]), max(scores[,1])) ,
-     ylim=c(min(scores[,2]), max(scores[,2])+(max(scores[,2])-min(scores[,2]))/4) 
-     ## Let a bit of room on top of the plot for legend
-)
-text(scores[,1], scores[,2] + 5, names(data2),cex=1.2)
-legend("top", horiz=T, legend=levels(as.factor(samplR)),fill=c(1,2,3,4,5),cex=1)
-##Dendogram
-plot(clusters,main="(c)",ylab="Euclidean Distance",xlab=NA,sub=NA)
 
 #######################################################################
 ## Step4: Pearson's Correlations ##
@@ -147,7 +102,7 @@ par(mfrow=c(1,3))
 corrplot(stage1.cor,method="number",tl.col="red",cl.lim=c(0,1),title="(a)",type="upper",diag=FALSE,mar=c(0,0,1,0))
 corrplot(stage2.cor,method="number",tl.col="red",cl.lim=c(0,1),title="(b)",type="upper",diag=FALSE,mar=c(0,0,1,0))
 corrplot(stage3.cor,method="number",tl.col="red",cl.lim=c(0,1),title="(c)",type="upper",diag=FALSE,mar=c(0,0,1,0))
-
+dev.off()
 
 #######################################################################
 ## Step5: DEG Analysis using Limma for selected replicates comparisons##
@@ -156,8 +111,8 @@ corrplot(stage3.cor,method="number",tl.col="red",cl.lim=c(0,1),title="(c)",type=
 data <- read.table("Second2017_counts.counts.matrix", header=T, row.names=1, com='')
 names(data)
 ##select replicates to be compared
-#For instance: 39FO versus 41QR
-col_ordering <- c(9,12,16,17)
+#For instance: 39EG versus 41HP
+col_ordering <- c(8,10,13,15)
 ###select subset of data from stages
 names(data)[col_ordering]
 rnaseqMatrix <- data[,col_ordering]
@@ -180,6 +135,7 @@ tmm_norm_factors <- calcNormFactors(rnaseqMatrix, method='TMM')
 ##MAkes a DGE List from counts and group indicators
 x <- DGEList(counts=rnaseqMatrix,group = conditions)
 # voom transformation for linear modeling of the counts data
+##transforms normalized counts to log2CPM values
 y <- voom(x, design, lib.size=lib_sizes*tmm_norm_factors, plot=T)
 ##Empirical Bayes for DE Analysis
 fit <- eBayes(contrasts.fit(lmFit(y,design),contrast.matrix))
@@ -206,9 +162,9 @@ m <- apply(c, 1, mean)
 tTags$logFC <- tTags$logFC  
 #setting results
 tTags2 <- cbind(tTags, logCPM=log2(m[rownames(tTags)]))
-DE_matrix <- data.frame(sampleA="39FO", sampleB="41QR", logFC=tTags$logFC, logCPM=tTags2$logCPM, PValue=tTags$'P.Value', FDR=tTags$'adj.P.Val')
+DE_matrix <- data.frame(sampleA="39EG", sampleB="41HP", logFC=tTags$logFC, logCPM=tTags2$logCPM, PValue=tTags$'P.Value', FDR=tTags$'adj.P.Val')
 rownames(DE_matrix) <- rownames(tTags)
-DE_matrix <- data.frame(sampleA="39FO", sampleB="41QR", logFC=tTags$logFC, logCPM=tTags2$logCPM, PValue=tTags$'P.Value', FDR=tTags$'adj.P.Val')
+DE_matrix <- data.frame(sampleA="39EG", sampleB="41HP", logFC=tTags$logFC, logCPM=tTags2$logCPM, PValue=tTags$'P.Value', FDR=tTags$'adj.P.Val')
 rownames(DE_matrix) <- rownames(tTags)
 
 plot_Volcano2 = function(data, xlab="log2(FoldChange)", ylab="-log10(FDR)", title="", pch=20,legendtext) {
@@ -218,7 +174,7 @@ plot_Volcano2 = function(data, xlab="log2(FoldChange)", ylab="-log10(FDR)", titl
   legend("topright",legend=legendtext,horiz=FALSE,col=c("red","orange","gray"),pch=20,cex=0.9)
 }
 
-plot_Volcano2(tTags,title="(i):39FO vs 41QR",legendtext=c("Upregulated in Stage 41","Upregulated in Stage 39","No Difference"))
+plot_Volcano2(tTags,title="(j):39EG vs 41HP",legendtext=c("Upregulated in Stage 41","Upregulated in Stage 39","No Difference"))
 
 ##counts for DEGs at different threshold levels of logFC
 DEG <- tTags[tTags$adj.P.Val < 0.05, ]
@@ -261,8 +217,8 @@ sum(duplicated(annotations[,2]))
 results <- DEG
 sum(duplicated(results[,1]))
 
-write.csv(results,"LimmaDEG-39FN41HP.csv")
-DEG <- read.csv("LimmaDEG-39FN41HP.csv")
+write.csv(results,"LimmaDEG-39EG41HP.csv")
+DEG <- read.csv("LimmaDEG-39EG41HP.csv")
 names(DEG)[1]<-"SeqName"
 final.data <- merge(annotations,DEG,by.x = "SeqName")
 check <- annotations
@@ -271,5 +227,5 @@ check2 <- check[with(check, SeqName %in% results$SeqName), ]
 annotations.new <-annotations[!duplicated(annotations[,2]),] 
 final.data.new <- merge(annotations.new,DEG,by.x = "SeqName")
 ##saving final DEG results with GO terms
-write.csv(final.data.new,"ResultsComparisonLimmaDEG-39FNvs41HP.csv")
+write.csv(final.data.new,"ResultsComparisonLimmaDEG-39EGvs41HJ.csv")
 
